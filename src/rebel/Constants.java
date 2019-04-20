73c7e3124d3d19646d2d7d92dc55b957acea8de3
Daniel Rolnik <danrol92@gmail.com>
2019-04-21 00:35:03 +0500

    Initial commit

---
+++ src/rebel/Constants.java
@@ -0,0 +1,7 @@
+package rebel;
+
+public class Constants {
+public static final int PLAYER_TO_START = 1;
+public static final int FIRST_PLAYER = 1;
+public static final int SECOND_PLAYER = 2;
+}
---
+++ src/rebel/Main.java
@@ -0,0 +1,11 @@
+package rebel;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		PlayGameController playGameController = new PlayGameController();
+		
+	}
+
+}
